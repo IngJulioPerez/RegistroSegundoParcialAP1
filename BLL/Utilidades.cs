2cbbf3e881acf196834e188bb81cc5fea2823584
IngJulioPerez <60108127+IngJulioPerez@users.noreply.github.com>
2020-07-06 16:54:25 -0400

    Add project files.

---
+++ BLL/Utilidades.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroSegundoParcialAP1.BLL
+{
+    public class Utilidades
+    {
+        public static int ToInt(String valor)
+        {
+            int retorno = 0;
+            int.TryParse(valor, out retorno);
+            return retorno;
+        }
+
+    }
+}
